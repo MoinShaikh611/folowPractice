0b06375a302db197f71e591648ba870fee6cac3d
MoinShaikh611 <imoin611@gmail.com>
2022-11-06 14:15:37 +0530

    dropdown and modal added in edit

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -1,23 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonService } from '../services/common.service';
 
 @Component({
   selector: 'app-roles',
   templateUrl: './roles.component.html',
-  styleUrls: ['./roles.component.css']
+  styleUrls: ['./roles.component.css'],
 })
-export class RolesComponent implements OnInit {
+export class RolesComponent implements OnInit, AfterViewInit {
   rolesList: any = [];
-  constructor(private commonService: CommonService) { }
+  constructor(private commonService: CommonService) {}
+
+  checkAll: any;
+  otherCheckBoxes: any;
 
   ngOnInit(): void {
     this.listAllRoles();
   }
 
-  listAllRoles(){
+  ngAfterViewInit(): void {}
+
+  onCheckAllChange(event: any) {
+    this.checkAll = document.getElementById(event.target.id);
+    this.otherCheckBoxes = document.querySelectorAll(
+      `input[type=checkbox]:not(#${event.target.id})`
+    );
+    setTimeout(() => {
+      this.otherCheckBoxes.forEach((input: any) => {
+        this.checkAll.checked == true
+          ? (input.checked = true)
+          : (input.checked = false);
+      });
+    }, 500);
+  }
+
+  listAllRoles() {
     this.commonService.getAllRoles().subscribe(
       (res: any) => {
-         this.rolesList = res.response;
+        this.rolesList = res.response;
       },
       (err) => {
         console.log(err);
@@ -25,15 +44,15 @@ export class RolesComponent implements OnInit {
     );
   }
 
-  deleteRole(role: any){
+  deleteRole(role: any) {
     let deleteRoleObj = {
       password: 'admin@1234',
     };
     let id = role.id;
-    this.commonService.roleDelete(deleteRoleObj,id).subscribe(
+    this.commonService.roleDelete(deleteRoleObj, id).subscribe(
       (res: any) => {
         console.log(res);
-        alert("Record Deleted Successfully");
+        alert('Record Deleted Successfully');
         this.listAllRoles();
       },
       (err: any) => {
@@ -42,4 +61,10 @@ export class RolesComponent implements OnInit {
     );
   }
 
+  toggleSelect(which_role: string) {
+    let element = document.querySelector(`.${which_role}`);
+    element?.parentElement?.classList.contains('active')
+      ? element?.parentElement?.classList.remove('active')
+      : element?.parentElement?.classList.add('active');
+  }
 }
